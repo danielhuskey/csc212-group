11e8c89ab56d03ada44b95cc598fae226146a00e
danielhuskey <79720949+danielhuskey@users.noreply.github.com>
2023-02-24 02:08:17 -0500

    Update gradebook.h
    added grades worth consts

--- gradebook.h
+++ gradebook.h
@@ -4,19 +4,27 @@
 class gradeBook{
     private:
         std::vector<std::vector<double>> gradeVector;
-        std::vector<std::string> nameVector;
-        std::string fName;
+        std::vector<std::string> nameVector={"Labs","Assignments","Projects","Exams"};
+        char * fName;
+        const double LABSWORTH =200;
+        const double ASSIGMENTSWORTH=200;
+        const double PROJECTSWORTH =500;
+        const double EXAMSWORTH =100;
+        
     public:
         //contructors
-        gradeBook(std::vector<std::vector<double>>tempGradeVector,std::vector<std::string> tempNameVector);
-        gradeBook(std::string fName);
+        gradeBook(std::vector<std::vector<double>>tempGradeVector,std::vector<std::string> tempNameVector, char * fName);
+        gradeBook(char * fName);
 
+        //print grades
         void printGrades();// prints in the terminal all of the grades
         void printGrades(std::string typeName);// prints in the terminal the gradestpye given
 
+        //saving the vector of grades back to the file
+        void saveGrades();//saves the grage Vector the file
 
 
-        //not done
+        //not done if worked on feel free to change what it takes as input if it makes it easy
         double findAvg(); //finds the avg of all grades 
         double findAvg(std::string typeName);// find avg of the type name
 
@@ -24,8 +32,8 @@ class gradeBook{
         void setGrades(std::string gradeName,int grade);
 
         
-        void saveGrades(std::string fName);//saves the grage Vector the file
 
+        
         
 
 
