c1ed598c77edfc530c1112e01cc8f557aabb4f90
Daniel Huskey <79720949+danielhuskey@users.noreply.github.com>
2023-02-26 02:43:44 -0500

    all methods are done to do constructor left

--- gradebook.h
+++ gradebook.h
@@ -6,10 +6,8 @@ class gradeBook{
         std::vector<std::vector<double>> gradeVector;
         std::vector<std::string> nameVector={"Labs","Assignments","Projects","Exams"};
         char * fName;
-        const double LABSWORTH =200;
-        const double ASSIGMENTSWORTH=200;
-        const double PROJECTSWORTH =500;
-        const double EXAMSWORTH =100;
+        const double WORTHS[4] ={200,200,500,100};
+     
         
     public:
         //contructors
@@ -18,27 +16,26 @@ class gradeBook{
 
         //print grades
         void printGrades();// prints in the terminal all of the grades
+
+        //ONLY CODE LEFT TO DO
         void printGrades(std::string typeName);// prints in the terminal the gradestpye given
 
         //saving the vector of grades back to the file
-        void saveGrades();//saves the grage Vector the file
+        void saveGrades();
 
-        //gets the grades at the given location
-        double getGrades(std::string gradeName,int loc);
-        //set the grade at the given location to new grade
-        void setGrades(std::string gradeName,int loc,double grade);
-    
-        //not done
+        //findAvg
         double findAvg(); //finds the avg of all grades 
         double findAvg(std::string typeName);// find avg of the type name
 
-        
+        //Finds the max val in each catigory
+        double findWorth(std::string typeName);
 
-        
+        //returns the grade at the given location
+        double getGrades(std::string gradeName,int loc);
 
-        
-        
+        //sets the old grade at the given location to the value of the new grade
+        void setGrades(std::string gradeName,int loc,double grade);
 
-
-
+        void addGrade(std::string gradeName,double grade);
+  
 };
