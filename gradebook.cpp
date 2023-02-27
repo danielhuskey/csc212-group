55798d945529f26fd92de1e446b0f63d5a5d2498
Daniel Huskey <79720949+danielhuskey@users.noreply.github.com>
2023-02-27 01:30:58 -0500

    made more readable + more comments

--- gradebook.cpp
+++ gradebook.cpp
@@ -12,36 +12,71 @@ gradeBook::gradeBook(std::vector<std::vector<double>> inputGradeVector,std::vect
 }
 // constructor normally used takes in a file and extrapualtes that into a 2D vector of doubles broken right now use the test constructor for now 
 gradeBook::gradeBook(char * file_name){
+    //setting the object var fname
+    this->fName = file_name;
+
+    //Opening file_name
     std::fstream openFile;
     openFile.open(file_name);
-    std::string temp;
+
+    //setting up temp vars
+    std::string inString;
     std::string toint;
-    std::vector<double> test;
+    std::vector<double> tempVect;
 
-    int index =1;
-    while (std::getline(openFile, temp)){
-       
-        
-        for(int x =0;x<temp.size();x++){
-            if(temp[x]==' '){
-                test.push_back(stod(toint));
+    //Getting lines from input_file
+    while (std::getline(openFile, inString)){
+       //Handling each line
+        for(int x =0;x<inString.size();x++){
+            //testing to see if location is a space to push that to the tempVect
+            if(inString[x]==' '){
+                tempVect.push_back(stod(toint));
                 toint.clear();
-                
+            // pushing the presplit char to a temp string    
             }else{
-                    toint.push_back(temp[x]);
-
+                    toint.push_back(inString[x]);
             }
         }
-        gradeVector.push_back(test);
-        test.clear();
-        index++;    
-
+        //testing if there is a space at the end of the inString
+        if(inString[inString.size()-1]!=' '){
+            //pushing last val to tempVect
+            tempVect.push_back(stod(toint));
+            //Clear toint for furture use
+            toint.clear();
+        }
+        
+        //Pushing tempVect to the man vect
+        gradeVector.push_back(tempVect);
+        //Clearing tempVect
+        tempVect.clear();
+        
 }
+//Close File
 openFile.close();
 }
 
+
+int gradeBook::breakName(std::string typeName){
+    int fristVal;
+    if(typeName =="Labs"){
+            fristVal = 0;
+        }else if(typeName =="Assignments"){
+            fristVal = 1;
+        }else if(typeName =="Projects"){
+            fristVal = 2;
+        }else if (typeName == "Exams"){
+            fristVal = 3;
+        }else{
+            std::cout<<"Grade Type Please use one of the fallowing Labs, Assignments, Projects, Exams"<<std::endl;
+            fristVal =-1;
+        }
+        return fristVal;
+}
+
+
 //prints out all of the grades that are in the "grade vector" seperating the prints by categoryName ie ("Labs","Assignment","Projects","Exams")
 void gradeBook::printGrades(){
+    //Looping through gradeVector and printing out grades
     for(int x =0;x<gradeVector.size();x++){
         //displays the category name followed by all of the grades in that category
         std::cout<<nameVector[x]<<": "<<std::endl;
@@ -55,7 +90,6 @@ void gradeBook::printGrades(){
 
 
 //prints out the grades in the categoryName ie ("Labs","Assignment","Projects","Exams") 
-
 void gradeBook::printGrades(std::string categoryName){
     int found;
     //looks for the category name in the known category vector to match to be able to only display that category
@@ -67,60 +101,39 @@ void gradeBook::printGrades(std::string categoryName){
 
     //couts all of the grades in the catigory found    
     std::cout<<"Your "<<nameVector[found]<<" total is "<<this->findAvg(nameVector[found])<<" / "<<WORTHS[found]<<std::endl;
-                                    //                ^ findAvg(<nameVector[found])   
+    
+    //Looping through subVector to print out
      for(int y =0; y<gradeVector[found].size();y++){
             std::cout<<nameVector[found]<<" "<<y<<": "<<gradeVector[found][y]<<" "<<std::endl;
         }   
 
 
 }
+
+//Find the max worth for the given Catigory
 double gradeBook::findWorth(std::string typeName){
-    int fristVal;
+    int fristVal =breakName(typeName);
         //finding the first val in the 2D vector by name
-        if(typeName =="Labs"){
-            fristVal = 0;
-        }else if(typeName =="Assignments"){
-            fristVal = 1;
-        }else if(typeName =="Projects"){
-            fristVal = 2;
-        }else if (typeName == "Exams"){
-            fristVal = 3;
-        }else{
-            std::cout<<"Grade Type: Please use one of the following Labs, Assignments, Projects, Exams"<<std::endl;
-            fristVal =-1;
-        }
+        
         if(fristVal!=-1){
-            
             return WORTHS[fristVal];
         }
+        
     return -1;
 }
 
-// addGrade takes 2 paramets and of the grade assingment and the grade. It then pushes back the user submitted number
+//Appending to type name grade
 void gradeBook::addGrade(std::string typeName,double grade){
-    int fristVal;
-    if(typeName =="Labs"){
-            fristVal = 0;
-        }else if(typeName =="Assignments"){
-            fristVal = 1;
-        }else if(typeName =="Projects"){
-            fristVal = 2;
-        }else if (typeName == "Exams"){
-            fristVal = 3;
-        }else{
-            std::cout<<"Grade Type: Please use one of the following Labs, Assignments, Projects, Exams"<<std::endl;
-            fristVal =-1;
-        }
-        if(fristVal!=-1){
-            
+    int fristVal =breakName(typeName);
+        if(fristVal!=-1){  
             gradeVector[fristVal].push_back(grade);
         }
-
-
 }
 
+//finds the avg of all grades 
 double gradeBook::findAvg(){
     double total;
+    //loops through the gradeVector to get a total
     for(int i =0;i<gradeVector.size();i++){
         for (int x=0;x<gradeVector[i].size();x++){
             total+=gradeVector[i][x];
@@ -130,74 +143,36 @@ double gradeBook::findAvg(){
 
 
 } 
-//finds the avg of all grades 
 
-//when it gets handled in main it does "that number / category worth" 
+// find avg of the type name
 double gradeBook::findAvg(std::string typeName){
-    int fristVal;
+    int fristVal=breakName(typeName);
     double total=0;
-    //finding the first val in the 2D vector by name
-    if(typeName =="Labs"){
-        fristVal = 0;
-    }else if(typeName =="Assignments"){
-        fristVal = 1;
-    }else if(typeName =="Projects"){
-        fristVal = 2;
-    }else if (typeName == "Exams"){
-        fristVal = 3;
-    }else{
-        std::cout<<"Grade Type: Please use one of the following Labs, Assignments, Projects, Exams"<<std::endl;
-        fristVal =-1;
-    }
-    if(fristVal!=-1){        
+
+    if(fristVal!=-1){
+        //looping through subVect to get total        
         for (int x=0;x<gradeVector[fristVal].size();x++){
             total+=gradeVector[fristVal][x];
-        }
-          
+        }    
     }
+
     return total;
-///Your Avg for labs is a 20/20
 
 }
-// find avg of the type name
 
 
 
+//gets and returns grade at a given location
 double gradeBook::getGrades(std::string gradeName,int loc){
 //gets the grade for given name for a grade
-    int fristVal;
-    //finding the first val in the 2D vector by name
-    if(gradeName =="Labs"){
-        fristVal = 0;
-    }else if(gradeName =="Assignments"){
-        fristVal = 1;
-    }else if(gradeName =="Projects"){
-        fristVal = 2;
-    }else if (gradeName == "Exams"){
-        fristVal = 3;
-    }else{
-        std::cout<<"Grade Type: Please use one of the following Labs, Assignments, Projects, Exams"<<std::endl;
-        fristVal =-1;
-    }
- 
+    int fristVal= breakName(gradeName);
     return gradeVector[fristVal][loc];
     
 }
+//sets grade a given point to new grade
 void gradeBook::setGrades(std::string gradeName,int loc,double grade){
-    int fristVal;
+    int fristVal= breakName(gradeName);
     //finding the first val in the 2D vector by name
-    if(gradeName =="Labs"){
-        fristVal = 0;
-    }else if(gradeName =="Assignments"){
-        fristVal = 1;
-    }else if(gradeName =="Projects"){
-        fristVal = 2;
-    }else if (gradeName == "Exams"){
-        fristVal = 3;
-    }else{
-        std::cout<<"Grade Type: Please use one of the following Labs, Assignments, Projects, Exams"<<std::endl;
-        fristVal =-1;
-    }
     if(fristVal!=-1){
         gradeVector[fristVal][loc]=grade;
     }
@@ -209,7 +184,7 @@ void gradeBook::saveGrades(){
     std::fstream openFile;
     //opens the file and clears it before resaving the new information back to the file
     openFile.open(fName, std::ofstream::out | std::ofstream::trunc);
-    
+
     //goes through the 2d vector and adds it to the file breaking line at the end of each vector to seperate the vector
     for(int y =0; y<gradeVector.size();y++){
         for(int x =0; x<gradeVector[y].size();x++){
@@ -221,4 +196,3 @@ void gradeBook::saveGrades(){
     openFile.close();
     
 }
-
